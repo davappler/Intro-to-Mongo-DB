2b966cd68d41601cec9a709a44121365f2407fc2
davibaweja <davibaweja@gmail.com>
2023-03-11 22:03:49 +0000

    added local databse connection back

--- nodeApp/db.js
+++ nodeApp/db.js
@@ -1,12 +1,12 @@
 const { MongoClient } = require("mongodb");
 
 let dbConnection;
-const uri =
-  "mongodb+srv://davappler:ArDmj0XLmeHuTYOs@cluster0.80hvizr.mongodb.net/?retryWrites=true&w=majority";
+// const uri =
+//   "mongodb+srv://davappler:ArDmj0XLmeHuTYOs@cluster0.80hvizr.mongodb.net/?retryWrites=true&w=majority";
 
 module.exports = {
   connectToDb: (cb) => {
-    MongoClient.connect(uri)
+    MongoClient.connect("mongodb://localhost:27017/bookstore")
       .then((client) => {
         dbConnection = client.db();
         return cb();
